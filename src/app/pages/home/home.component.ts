9893e232f7d72d9f0b34b8faa00559209bae927a
Emma Jaques <emma@uhray.com>
2018-06-21 21:20:32 +0100

    New Updates

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,28 +8,51 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 posts = [
    {
-      myname: 'Father Jenkins',
+      myname: 'Name Namely',
       thedate: 'May 13th 2013',
       caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque cursus urna a nisi hendrerit volutpat. Donec varius sapien et lorem laoreet ornare. Suspendisse convallis velit et purus dictum auctor. Etiam aliquet blandit lorem, eu placerat augue porttitor ut. Cras vitae turpis ornare, pulvinar massa non, sollicitudin ligula.',
       profileUrl: 'http://www.leadersmag.com/issues/2013.1_Jan/ROB/LEADERS-Rev-John-Jenkins-University-of-Notre-Dame-web-images/LEADERS-Rev-John-Jenkins-University-of-Notre-Dame.jpeg',
-      imageUrl: 'https://scontent.fash1-1.fna.fbcdn.net/v/t1.0-9/33814913_1577979828994199_4316850570984947712_n.jpg?_nc_cat=0&oh=44ebb70399135c218f3d1a195b1c834f&oe=5B7C2456'
+      imageUrl: 'https://images.huffingtonpost.com/2016-07-13-1468418042-7917061-TouchdownJesusAgain_art-thumb.jpg'
     
    },
    {
-      myname: 'Father Jenkins 2',
+      myname: 'Father Jenkins',
       thedate: 'May 2nd 2013',
-      caption: 'some other post caption',
+      caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque cursus urna a nisi hendrerit volutpat. Donec varius sapien et lorem laoreet ornare. Suspendisse convallis velit et purus dictum auctor.',
       profileUrl: 'http://www.leadersmag.com/issues/2013.1_Jan/ROB/LEADERS-Rev-John-Jenkins-University-of-Notre-Dame-web-images/LEADERS-Rev-John-Jenkins-University-of-Notre-Dame.jpeg',
       imageUrl: 'https://images.huffingtonpost.com/2016-07-13-1468418042-7917061-TouchdownJesusAgain_art-thumb.jpg'
-      
+
    }
 ]
   constructor() { 
 
   }
+  myForm: FormGroup;
+  items: any[] = [];
 
   ngOnInit() {
+    this.myForm = this.formBuilder.group({
+    myname: '',
+    thedate: '',
+    caption: '',
+    profileUrl: '',
+    imageUrl: ''
+  });
   }
+
+  createItem(): FormGroup {
+  return this.formBuilder.group({
+    myname: '',
+    thedate: '',
+    caption: '',
+    profileUrl: '',
+    imageUrl: ''
+  });
+
+  addItem(): void {
+  	this.items;
+  }
+}
  
 }
 
